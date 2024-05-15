7913e1ddface981a2cdb34c58b97040775c6c8b1
Guillermo Rodríguez <guillebep@gmail.com>
2024-05-15 13:21:00 +0200

    Cleanup and add docstring

--- ws_multiple_subs.py
+++ ws_multiple_subs.py
@@ -16,14 +16,16 @@ class SubscriptionHandler:
     def __init__(self, wss_url):
         self.wss_url = wss_url
 
-    async def process_subscriptions(self):
+    async def process_subscriptions(self) -> None:
+        """
+        Performs the websocket connection and processes the subscriptions and calls the callbacks
+        :return: None
+        """
         async for self.w3_socket in AsyncWeb3(WebSocketProvider(self.wss_url)):
-            print(f"process subs {self.w3_socket}")
             try:
                 async for message in self.w3_socket.socket.process_subscriptions():
-                    print(f"{message=}")
                     try:
-                        self.callbacks[message['subscription']](message['result'])
+                        self.callbacks[message["subscription"]](message["result"])
                     except ValueError as e:
                         try:
                             print(f"Callback for {message['subscription']} not found")
@@ -37,40 +39,64 @@ class SubscriptionHandler:
                     await self.w3_socket.eth.unsubscribe(sub_id)
                 break
 
-    async def subscribe(self, callback: callable,
-                        event_type: SubscriptionType, **event_params):
-        if self.w3_socket is not None:
+    async def subscribe(
+        self, callback: callable, event_type: SubscriptionType, **event_params
+    ) -> None:
+        """
+        Subscribes to the given event type with the given callback.
+        Must be called while process_subscriptions() task is running
+
+        :param callback: The function to call when the event is received
+        :param event_type: The event type to subscribe to
+        :param event_params: Additional parameters to pass to the subscription
+        :return: None
+        """
+        if self.is_connected():
             sub_id = await self.w3_socket.eth.subscribe(event_type, event_params)
             print(f"Subscribed to {sub_id}")
             self.callbacks[sub_id] = callback
         else:
-            raise RuntimeError("Websocket connection not established, it's not possible to subscribe")
+            raise RuntimeError(
+                "Websocket connection not established, it's not possible to subscribe"
+            )
 
-    async def unsubscribe(self, sub_id: HexStr):
-        if self.w3_socket is not None:
+    async def unsubscribe(self, sub_id: HexStr) -> None:
+        """
+        Unsubscribes from a subscription identified by sub_id.
+        Must be called while process_subscriptions() task is running
+
+        :param sub_id: The subscription ID to unsubscribe from
+        :return: None
+        """
+        if self.is_connected():
             await self.w3_socket.eth.unsubscribe(sub_id)
             self.callbacks.pop(sub_id)
         else:
-            raise RuntimeError("Websocket connection not established, it's not possible to unsubscribe")
+            raise RuntimeError(
+                "Websocket connection not established, it's not possible to unsubscribe"
+            )
 
-    def is_connected(self):
-        print(f"{self.w3_socket=}")
+    def is_connected(self) -> bool:
         return self.w3_socket is not None
 
 
 def callback_logs(message):
     print(f"New log received: {message}")
 
+
 def callback_heads(message):
     print(f"New header received: {message}")
+
 
 async def main():
     subs_handler = SubscriptionHandler("wss://eth.drpc.org")
     sub_task = asyncio.create_task(subs_handler.process_subscriptions())
     while not subs_handler.is_connected():
         await asyncio.sleep(1)
-    await subs_handler.subscribe(callback_logs, 'logs', address='0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc')
-    await subs_handler.subscribe(callback_heads, 'newHeads')
+    await subs_handler.subscribe(
+        callback_logs, "logs", address="0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc"
+    )
+    await subs_handler.subscribe(callback_heads, "newHeads")
     try:
         while True:
             await asyncio.sleep(0)
@@ -78,5 +104,6 @@ async def main():
         sub_task.cancel()
         await sub_task
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
